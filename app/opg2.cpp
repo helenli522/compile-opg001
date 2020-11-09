83f103464d871f5e097fa0c8aa64ef555d58e2c7
helenli522 <60768470+helenli522@users.noreply.github.com>
2020-11-09 12:49:52 +0800

    debug

--- app/opg2.cpp
+++ app/opg2.cpp
@@ -3,91 +3,115 @@
 #include<stack>
 #define MAX 1024
 using namespace std;
-char ch[6] = {'+', '*', 'i', '(', ')'};
+char ch[6] = {'+', '*', 'i', '(', ')','#'};
 char p[6][6];
+char line[MAX];
 stack<int> s1,s2;
 
-int get(int c);
-int r();
+void init();
+char find_lt();
+int def(int c);
+void analyse();
 
 int main(int argc,char* argv[])
 {
+    int len = 0;
+    char c;
     char* fname = argv[1];
-    char line[MAX];
     FILE* in;
-
     in = fopen(fname,"r");
-    while(!s1.empty()) s1.pop();
-    while(!s2.empty()) s2.pop();
-    //>:1 =:0 <:-1
-    for(int i=0; i<5; i++)
-        for(int j=0; j<5; j++) p[i][j]=2;
-    p[0][0]=p[0][4]=p[1][0]=p[1][1]=p[1][4]=p[2][0]=p[2][1]=p[2][4]=p[4][0]=p[4][1]=p[4][4]=1;
-    p[3][4]=0;
-    p[0][1]=p[0][2]=p[0][3]=p[1][2]=p[1][3]=p[3][0]=p[3][1]=p[3][2]=p[3][3]=-1;
-
-    char c=fgetc(in);
-    while(c!=EOF){
-        if(get(c)==-1) break;
-        if(s1.empty()) {s1.push(c); printf("I%c\n",c); c=fgetc(in);}
-        else{
-            int a=get(s1.top()),b=get(c);
-            if(b==-1) {printf("E\n"); break;}
-            if(p[a][b]==2) {printf("E\n"); break;}
-            if(p[a][b]<=0) {s1.push(c); printf("I%c\n",c); c=fgetc(in);}
-            else{
-                int flag=r();
-                if(flag==0) printf("R\n");
-                else {printf("RE\n"); break;}
-            }
-        }
-        while(get(c)==-1){
-            int flag=r();
-            if(flag==0) printf("R\n");
-            else {printf("RE\n"); break;}
-            if(s1.empty()) break;
-        }
+    while((c=fgetc(in))!=EOF){
+        if(c!='\r' && c!='\n') line[len++] = c;
     }
+    line[len++] = '#'; //printf("%s\n",line);
     fclose(in);
+    init();
+    analyse();
 	return 0;
 }
 
-int get(int c){
+void init()
+{
+    for(int i=0; i<6; i++) for(int j=0; j<6; j++) p[i][j]=2;
+    p[0][0]=p[0][4]=p[0][5]=p[1][0]=p[1][1]=p[1][4]=p[1][5]=p[2][0]=p[2][1]=p[2][4]=p[2][5]=p[4][0]=p[4][1]=p[4][4]=p[4][5]=1;
+    p[3][4]=p[5][5]=0;
+    p[0][1]=p[0][2]=p[0][3]=p[1][2]=p[1][3]=p[3][0]=p[3][1]=p[3][2]=p[3][3]=p[5][0]=p[5][1]=p[5][2]=p[5][3]=-1;
+}
+char find_lt()
+{
+    char c,ans;
+    c=s1.top();
+    if(def(c)<6)
+        return c;
+    s1.pop();
+    ans=s1.top();
+    s1.push(c);
+    return ans;
+}
+int def(int c)
+{
     if(c=='+') return 0;
     if(c=='*') return 1;
     if(c=='i') return 2;
     if(c=='(') return 3;
     if(c==')') return 4;
-    return -1;
+    if(c=='#') return 5;
+    return 6;
 }
-int r(){
-    int a=s1.top();
-    if(a=='(') return -1;
-    if(a=='i'){
-        s1.pop();
-        s2.push('N');
-        return 0;
-    }
-    if(a=='+'){
-        if(s2.size()>=2){
-            s1.pop();
-            s2.pop();
-            return 0;
+void analyse()
+{
+    s1.push('#');
+    char rc,lc;
+    int a,b,compare;
+    for(int i=0;i<strlen(line);i++){
+        rc=line[i]; //printf("rc:%c\n",rc);
+        if(def(rc)<6){
+            lc=find_lt(); //printf("lc:%c\n",lc);
+            a=def(lc); //printf("a:%d\n",a);
+            b=def(rc); //printf("b:%d\n",b);
+            compare=p[a][b];
+            if(compare==2){
+                printf("E\n");
+                return;
+            }
+            if(compare<=0&&rc!='#'){
+                s1.push(rc);
+                printf("I%c\n",rc);
+            }
+            else if(rc=='#'&&lc=='#'){
+                return;
+            }
+            else{
+                if(lc=='i'){ // ? lc or s1.top()
+                    s1.pop();
+                    s1.push('N');
+                }
+                else{
+                    if(s1.size()>=4){
+                        char t[4];
+                        t[2]=s1.top(); s1.pop(); //printf("t[2]:%c\n",t[2]);
+                        t[1]=s1.top(); s1.pop(); //printf("t[1]:%c\n",t[1]);
+                        t[0]=s1.top(); s1.pop(); //printf("t[0]:%c\n",t[0]);
+                        t[3]='\0';
+                        if(strcmp("N+N",t)==0 || strcmp("N*N",t)==0 || strcmp("(N)",t)==0)
+                            s1.push('N');
+                        else{
+                            printf("RE\n");
+                            return;
+                        }
+                    }
+                    else{
+                        printf("RE\n");
+                        return;
+                    }
+                }
+                i--;
+                printf("R\n");
+            }
         }
-        else return -1;
-    }
-    if(a=='*'){
-        if(s2.size()>=2){
-            s1.pop();
-            s2.pop();
-            return 0;
+        else{
+            printf("E\n");
+            return;
         }
-        else return -1;
-    }
-    if(a==')'){
-        s1.pop();
-        if(s1.empty() || s1.top()!='(') return -1;
-        s1.pop();
-        return 0;
     }
 }
